8d1575c82d6fc2006c3c75e64b64eb1af6df7ce5
cristhofer <cjtm93@outlook.com>
2019-04-03 10:31:15 -0500

    tabla departamento

--- database/migrations/2019_04_02_111510_create_departamento_table.php
+++ database/migrations/2019_04_02_111510_create_departamento_table.php
@@ -18,6 +18,10 @@ class CreateDepartamentoTable extends Migration
             $table->string('nombre_departamento');
             $table->string('descripcion_departamento');
             $table->char('estado_departamento',1)->default(1);
+            $table->string('horario_inicio');
+            $table->string('horario_fin');
+            $table->time('hora_inicio');
+            $table->time('hora_fin');
         });
     }
 
