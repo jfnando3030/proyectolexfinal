b1cd835111eb0d82c1738bff75553aefbdf04c73
gmocha <gmocha14@gmail.com>
2019-04-03 18:11:40 -0500

    cargando archivos para el modulo solicitud 5

---
+++ resources/views/administracion/gestionar/actualizar.blade.php
@@ -0,0 +1,82 @@
+@extends('layouts.administracion')
+
+@section('contenido')
+
+    <div align="center" class="col-md-12 col-12 col-xs-12 col-lg-12 col-sm-12" style="padding-top:15px; padding-bottom: 25px">
+        <h4 style="color: black; text-align:center; font-size:25px;"> Gestionar casos con abogado </h4>
+    </div>
+
+  
+    <div class="container-fluid">
+
+        @if (session('mensaje-registro'))
+            @include('mensajes.msj_correcto')
+        @endif
+
+        <div class="emp-profile" style="padding: 3%;">
+            <form action="{{ route('actualizar_abogado_caso') }}" method="put">
+                <input type="hidden" name="_token" value="{{ csrf_token() }}" id="token">
+
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-12 col-12 col-lg-12 col-sm-12 col-xs-12">
+                                <div class="tab-content profile-tab" id="myTabContent">
+                                    <div class="tab-pane fade show active" id="home" role="tabpanel" aria-labelledby="home-tab">
+                                    	<input type="hidden" name="id" id="id" value=" {{ $solicitud[0]->id }} ">
+                                        <div class="row">
+                                            <div class="col-md-12" style="padding-bottom: 15px;">
+                                                <label>Nombres del caso:</label>
+                                                <hr>
+                                                <p style="color: gray;"> {{ $solicitud[0]->nombre_solicitud }}" </p>
+                                                <hr>
+                                            </div>
+
+                                           
+                                        </div>
+                                        <div class="row">
+                                            <div class="col-md-6" style="padding-bottom: 15px;">
+                                                <label>Abogado a cargo:</label>
+                                                <p style="color: gray;"> {{ $abogado[0]->nombres }} {{ $abogado[0]->apellidos }} </p>
+                                                <hr>
+                                            </div>
+                                            <div class="col-md-6" style="padding-bottom: 15px;">
+                                                <label> Cambiar de abogado:</label>
+                                                <select class="form-control" id="abogado" name="abogado" required="">
+                                                    <option value=""> Seleccione un abogado</option>
+                                                    @foreach($abogados as $abogados)
+                                                        <option value="{{ $abogados->id }}"> {{ $abogados->nombres }} {{ $abogados->apellidos }} </option>
+                                                    @endforeach
+                                                </select> 
+                                            </div>
+                                           
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+
+                        </div>
+
+                            <div class="row">
+                                <div class="col-md-4">
+                            </div>
+
+                            <div class="col-md-4" style="padding-bottom: 15px;">
+                                {!! Form::submit('Guardar información',['class'=>'btn btn-primary btn-block']) !!}
+                            </div>
+
+                            <div class="col-md-4">
+                            
+                            </div>
+                        </div>
+                    </div>              
+                </div>
+           </form>
+        </div>
+    </div>
+
+@endsection
+
+@section('script')
+    <script src="{{url('administration/dist/js/validaNumerosLetras.js')}}"></script>
+@endsection
--- resources/views/layouts/administracion.blade.php
+++ resources/views/layouts/administracion.blade.php
@@ -214,6 +214,24 @@
 
                         @endif
 
+                        @if(Auth::user()->rol == "Administrador")
+                        <li class="has-sub">
+                            <a class="js-arrow" href="#" id="menu_letras">
+                                <i class="fas fa-folder"></i>Gestion
+                                <span class="arrow">
+                                    <i class="fas fa-angle-down"></i>
+                                </span>
+                            </a>
+                            <ul class="list-unstyled navbar__sub-list js-sub-list">
+                                <li>
+                                    <a id="menu_letras" href="{{url('administracion/gestionar/casos/listado')}}" onclick="return myFunction();">
+                                        <i class="fas fa-file"></i>Casos</a>
+                                </li>
+                            </ul>
+                        </li>
+
+                        @endif
+
                         <li>
                             <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form2').submit();" id="menu_letras"><i class="fas fa-sign-out-alt"></i> Cerrar sesión</a>
 
