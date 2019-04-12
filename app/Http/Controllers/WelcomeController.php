9f406efd28e5a46ea78d4865534cd850b8bee5d2
gmocha <gmocha14@gmail.com>
2019-04-12 11:55:53 -0500

    cargando archivos para modulo pago 9

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -420,10 +420,170 @@ class WelcomeController extends Controller
 
     public function actualizar_abogado_caso(Request $request)
     {
+
       $casos = Solicitud::findOrFail($request->id);  
       $casos->id_user_abogado = $request->abogado;
+
+      $abogado = User::findOrFail($request->abogado);
+
+      $cliente = User::findOrFail($casos->id_user_solicitud);  
+
       if($casos->save()){
-            return redirect('administracion/gestionar/casos/listado')->with('mensaje-registro', 'Datos actualizados correctamente.');
+
+        // NOTIFICA AL ABOGADO
+
+        $emaiL_abogado =$abogado->email;
+
+        $nombres_abogado = $abogado->nombres . ' ' . $abogado->apellidos;
+
+        $nombres_cliente = $cliente->nombres . ' ' . $cliente->apellidos;
+
+        $codigohtml = '
+            <head>
+                <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,700" rel="stylesheet">
+                <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700" rel="stylesheet">
+             </head>
+
+             <body style="background-color:white;">
+                <div style="width:100%; background-color:#003364; text-align:center;">
+                <br>    
+                    <div style="width:550px;margin:50px auto; background-color:white;">
+                        <div style="width:100%; padding-top:35px; padding-bottom:15px;">
+                            <img width="213" border="0" style="width: 213px;" src="http://35.237.74.133/frontend/images/redondo.png" alt="" />
+                        </div>
+                        <div style="width:100%; color: #343434; font-size: 24px; font-family: Quicksand, Calibri, sans-serif; font-weight:700;letter-spacing: 3px; line-height: 35px;">
+                            <p style="color:#999; line-height:0px;margin-top:-15px;">_</p>
+                        </div>
+                        <div style="color: #888888; font-size: 16px; font-family: Work Sans, Calibri, sans-serif; line-height: 24px; width:400px; margin:30px auto; text-align:center;">
+
+                          <p style="font-family: Verdana, Geneva, sans-serif; font-size: 12px; text-align:justify"> Hola ' . $nombres_abogado  . ', te escribimos para notificarte que se te ha asignado el caso: ' . $casos->nombre_solicitud . ', el cual pertenece a ' . $nombres_cliente  . '.</p>
+
+                          <p style="font-family: Verdana, Geneva, sans-serif; font-size: 12px; text-align:justify">&iexcl;Un abrazo!,<br/>
+                             El equipo de LEX 4.0<br/>
+                          <p style="font-family: Verdana, Geneva, sans-serif; font-size: 11px;"><span><strong>ESTA ES UNA COMUNICACI&Oacute;N AUTOM&Aacute;TICA, POR FAVOR NO RESPONDER.</strong></span></p>
+                        </div>
+                            
+                        <div style="width:550px;margin:0px auto; color:#999; font-family: Work Sans, Calibri, sans-serif;background-color: dimgray; padding-top:10px; padding-bottom:10px;">
+                            <table style="margin:auto auto;">
+                                <tr>
+                                    <td width="90" style="color:#999; font-family: Work Sans, calibri, sans-serif;font-size:13px; font-weight:bold;">
+                                        <p style="color: white; ">S&iacute;guenos:</p>
+                                    </td>               
+                                    <td width="250">
+                                    <p><a href="https://www.twitter.com/BangoEc" target="_blank"><img width="50" src="http://download.seaicons.com/download/i49182/yootheme/social-bookmark/yootheme-social-bookmark-social-twitter-button-blue.ico" alt="twitter" style="margin-right:15px;"/></a>
+                                    <a href="https://www.facebook.com/BangoEnergyGel/" target="_blank"><img width="50" src="https://cdn0.iconfinder.com/data/icons/yooicons_set01_socialbookmarks/512/social_facebook_button_blue.png" alt="twitter" style="margin-right:15px;"/></a>
+                                    <a href="https://www.instagram.com/bangoenergygel/" target="_blank"><img width="50" src="https://3.bp.blogspot.com/-PLtBjidnB-o/W5Ap8VkGNTI/AAAAAAAAAv4/5WmcJdRBWN0ut_7Kuq5AI1E_di6XaRh3ACLcBGAs/s1600/instagram-colourful-icon.png" alt="twitter"/></a></p>
+                                    </td>
+                                </tr>
+                            </table>            
+                        </div>
+                    </div>
+                    <div style="width:550px;margin:0px auto; color:#666; font-family: Work Sans, Calibri, sans-serif;font-size:12px;">
+                        Esta es una comunicaci&oacute;n autom&aacute;tica, no responder a esta direcci&oacute;n de correo.
+                    </div>
+                </div>
+            </body>
+        ';
+
+        $mail = new PHPMailer(true);
+        $mail->IsSMTP();
+        $mail->SMTPDebug = 0;
+        $mail->SMTPAuth = true; //'login'
+        $mail->SMTPSecure = 'ssl';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 465;              
+        $mail->IsHTML(true);
+        $mail->CharSet = 'UTF-8';
+
+        //confirmaciones
+        $mail->Username = 'gerencia@geomarvaldez.com';
+        $mail->Password = 'Geomar2018';
+        $mail->SetFrom('gerencia@geomarvaldez.com', 'Notificaciones LEX 4.0');
+        $mail->Subject = html_entity_decode('Cambio de abogado');
+            
+        $mail->Body = $codigohtml;      
+        $mail->AddAddress($emaiL_abogado);
+        $mail->Send();
+
+
+        // NOTIFICANDO AL CLIENTE 
+
+         // NOTIFICA AL ABOGADO
+
+        $emaiL_cliente =$cliente->email;
+
+        $codigohtml = '
+            <head>
+                <link href="https://fonts.googleapis.com/css?family=Quicksand:300,400,700" rel="stylesheet">
+                <link href="https://fonts.googleapis.com/css?family=Work+Sans:300,400,500,600,700" rel="stylesheet">
+             </head>
+
+             <body style="background-color:white;">
+                <div style="width:100%; background-color:#003364; text-align:center;">
+                <br>    
+                    <div style="width:550px;margin:50px auto; background-color:white;">
+                        <div style="width:100%; padding-top:35px; padding-bottom:15px;">
+                            <img width="213" border="0" style="width: 213px;" src="http://35.237.74.133/frontend/images/redondo.png" alt="" />
+                        </div>
+                        <div style="width:100%; color: #343434; font-size: 24px; font-family: Quicksand, Calibri, sans-serif; font-weight:700;letter-spacing: 3px; line-height: 35px;">
+                            <p style="color:#999; line-height:0px;margin-top:-15px;">_</p>
+                        </div>
+                        <div style="color: #888888; font-size: 16px; font-family: Work Sans, Calibri, sans-serif; line-height: 24px; width:400px; margin:30px auto; text-align:center;">
+
+                          <p style="font-family: Verdana, Geneva, sans-serif; font-size: 12px; text-align:justify"> Hola ' . $nombres_cliente  . ', te escribimos para notificarte que tu caso: ' . $casos->nombre_solicitud . ' ha sido asignado al Abogado ' . $nombres_abogado  . '.</p>
+
+                          <p style="font-family: Verdana, Geneva, sans-serif; font-size: 12px; text-align:justify">&iexcl;Un abrazo!,<br/>
+                             El equipo de LEX 4.0<br/>
+                          <p style="font-family: Verdana, Geneva, sans-serif; font-size: 11px;"><span><strong>ESTA ES UNA COMUNICACI&Oacute;N AUTOM&Aacute;TICA, POR FAVOR NO RESPONDER.</strong></span></p>
+                        </div>
+                            
+                        <div style="width:550px;margin:0px auto; color:#999; font-family: Work Sans, Calibri, sans-serif;background-color: dimgray; padding-top:10px; padding-bottom:10px;">
+                            <table style="margin:auto auto;">
+                                <tr>
+                                    <td width="90" style="color:#999; font-family: Work Sans, calibri, sans-serif;font-size:13px; font-weight:bold;">
+                                        <p style="color: white; ">S&iacute;guenos:</p>
+                                    </td>               
+                                    <td width="250">
+                                    <p><a href="https://www.twitter.com/BangoEc" target="_blank"><img width="50" src="http://download.seaicons.com/download/i49182/yootheme/social-bookmark/yootheme-social-bookmark-social-twitter-button-blue.ico" alt="twitter" style="margin-right:15px;"/></a>
+                                    <a href="https://www.facebook.com/BangoEnergyGel/" target="_blank"><img width="50" src="https://cdn0.iconfinder.com/data/icons/yooicons_set01_socialbookmarks/512/social_facebook_button_blue.png" alt="twitter" style="margin-right:15px;"/></a>
+                                    <a href="https://www.instagram.com/bangoenergygel/" target="_blank"><img width="50" src="https://3.bp.blogspot.com/-PLtBjidnB-o/W5Ap8VkGNTI/AAAAAAAAAv4/5WmcJdRBWN0ut_7Kuq5AI1E_di6XaRh3ACLcBGAs/s1600/instagram-colourful-icon.png" alt="twitter"/></a></p>
+                                    </td>
+                                </tr>
+                            </table>            
+                        </div>
+                    </div>
+                    <div style="width:550px;margin:0px auto; color:#666; font-family: Work Sans, Calibri, sans-serif;font-size:12px;">
+                        Esta es una comunicaci&oacute;n autom&aacute;tica, no responder a esta direcci&oacute;n de correo.
+                    </div>
+                </div>
+            </body>
+        ';
+
+        $mail = new PHPMailer(true);
+        $mail->IsSMTP();
+        $mail->SMTPDebug = 0;
+        $mail->SMTPAuth = true; //'login'
+        $mail->SMTPSecure = 'ssl';
+        $mail->Host = 'smtp.gmail.com';
+        $mail->Port = 465;              
+        $mail->IsHTML(true);
+        $mail->CharSet = 'UTF-8';
+
+        //confirmaciones
+        $mail->Username = 'gerencia@geomarvaldez.com';
+        $mail->Password = 'Geomar2018';
+        $mail->SetFrom('gerencia@geomarvaldez.com', 'Notificaciones LEX 4.0');
+        $mail->Subject = html_entity_decode('Cambio de abogado');
+            
+        $mail->Body = $codigohtml;      
+        $mail->AddAddress($emaiL_cliente);
+        $mail->Send();
+
+
+
+
+
+        return redirect('administracion/gestionar')->with('mensaje-registro', 'Datos actualizados correctamente.');
         }
     }
 
@@ -532,7 +692,7 @@ class WelcomeController extends Controller
 
                           <p style="font-family: Verdana, Geneva, sans-serif; font-size: 12px; text-align:justify"> Para ver la respuesta da clic en el siguiente enlace.</p>
 
-                          <a href="http://localhost/proyectolexfinal/administracion/respuesta/registrar/' . $respuesta->id . '" style="font-family: Verdana, Geneva, sans-serif; font-size: 12px; text-align:center"> Da clic en este enlace </a>
+                          <a href="http://localhost/proyectolexfinal/administracion/casos/respuesta/' . Crypt::encrypt($respuesta->id ) . '" style="font-family: Verdana, Geneva, sans-serif; font-size: 12px; text-align:center"> Da clic en este enlace </a>
 
                           <p style="font-family: Verdana, Geneva, sans-serif; font-size: 12px; text-align:justify">&iexcl;Un abrazo!,<br/>
                              El equipo de LEX 4.0<br/>
